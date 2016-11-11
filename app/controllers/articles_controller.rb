db4ddb5b3578f78dd1ba7200a81c861c2718d660
User <user@user.office.flatstack.com>
2016-11-11 20:09:27 +0300

    Add action, views and partials for create, update and delete article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,21 +13,37 @@ class ArticlesController < ApplicationController
   end
 
   def edit
+    @article = Article.find(params[:id])
   end
 
   def update
     @article = Article.find(params[:id])
     @atricle.update_attributes(article_params)
+
+    flash[:notice] = "Article was updated"
+    redirect_to article_path(@article)
   end
 
   def new
+    @article = Article.new
   end
 
   def create
+    @article = User.first.articles.new(article_params)
+    if @article.save
+      flash[:notice] = "Article was created"
+      redirect_to article_path(@article)
+    else
+      flash[:error] = "Something went wrong"
+      render "new"
+    end
   end
 
   def destroy
     @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to articles_path
   end
 
   def my_sum(a, b)
